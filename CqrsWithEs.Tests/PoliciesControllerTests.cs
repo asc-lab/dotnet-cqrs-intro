5d7567e00996fc921d09885ed9b09056ec3d0b2a
wojtek.suwala@outlook.com <wojtek.suwala@outlook.com>
2019-02-27 15:19:49 +0100

    tests added for all rest api methods

--- CqrsWithEs.Tests/PoliciesControllerTests.cs
+++ CqrsWithEs.Tests/PoliciesControllerTests.cs
@@ -33,5 +33,111 @@ namespace CqrsWithEs.Tests
             Assert.NotEmpty(response.Data.PolicyNumber);
         }
 
+        [Fact]
+        public async Task CanBuyAdditionalCover()
+        {
+            var client = factory.CreateClient();
+
+            var createPolicyResponse = await client.DoPostAsync<CreatePolicyResult>("/api/Policies", new CreatePolicyCommand
+            {
+                OfferNumber = "OFF001",
+                PurchaseDate = new DateTime(2019,2,26),
+                PolicyStartDate = new DateTime(2019,2,27)
+            });
+
+
+            var buyCoverResponse = await client.DoPostAsync<BuyAdditionalCoverResult>("/api/Policies/BuyAdditionalCover", new BuyAdditionalCoverCommand
+            {
+               PolicyId = createPolicyResponse.Data.PolicyId,
+                NewCoverCode = "AC",
+                NewCoverPrice = 100M,
+                NewCoverPriceUnit = TimeSpan.FromDays(365),
+                EffectiveDateOfChange = new DateTime(2019,6,1)
+            });
+            
+            Assert.Equal(2, buyCoverResponse.Data.VersionWithAdditionalCoversNumber);
+        }
+        
+        [Fact]
+        public async Task CanConfirmBuyAdditionalCover()
+        {
+            var client = factory.CreateClient();
+
+            var createPolicyResponse = await client.DoPostAsync<CreatePolicyResult>("/api/Policies", new CreatePolicyCommand
+            {
+                OfferNumber = "OFF001",
+                PurchaseDate = new DateTime(2019,2,26),
+                PolicyStartDate = new DateTime(2019,2,27)
+            });
+
+
+            var buyCoverResponse = await client.DoPostAsync<BuyAdditionalCoverResult>("/api/Policies/BuyAdditionalCover", new BuyAdditionalCoverCommand
+            {
+                PolicyId = createPolicyResponse.Data.PolicyId,
+                NewCoverCode = "AC",
+                NewCoverPrice = 100M,
+                NewCoverPriceUnit = TimeSpan.FromDays(365),
+                EffectiveDateOfChange = new DateTime(2019,6,1)
+            });
+            
+            var confirmBuyCoverResponse = await client.DoPostAsync<ConfirmBuyAdditionalCoverResult>("/api/Policies/ConfirmBuyAdditionalCover", new ConfirmBuyAdditionalCoverCommand
+            {
+                PolicyId = createPolicyResponse.Data.PolicyId,
+                VersionToConfirmNumber = buyCoverResponse.Data.VersionWithAdditionalCoversNumber
+            });
+            
+            Assert.Equal(2, confirmBuyCoverResponse.Data.VersionConfirmed);
+        }
+
+        [Fact]
+        public async Task CanTerminatePolicy()
+        {
+            var client = factory.CreateClient();
+
+            var createPolicyResponse = await client.DoPostAsync<CreatePolicyResult>("/api/Policies", new CreatePolicyCommand
+            {
+                OfferNumber = "OFF001",
+                PurchaseDate = new DateTime(2019,2,26),
+                PolicyStartDate = new DateTime(2019,2,27)
+            });
+
+
+            var terminateResponse = await client.DoPostAsync<TerminatePolicyResult>("/api/Policies/Terminate", new TerminatePolicyCommand
+            {
+                PolicyId = createPolicyResponse.Data.PolicyId,
+                TerminationDate = new DateTime(2019,6,1)
+            });
+            
+            Assert.Equal(2, terminateResponse.Data.VersionWithTerminationNumber);
+        }
+        
+        [Fact]
+        public async Task CanConfirmTerminationPolicy()
+        {
+            var client = factory.CreateClient();
+
+            var createPolicyResponse = await client.DoPostAsync<CreatePolicyResult>("/api/Policies", new CreatePolicyCommand
+            {
+                OfferNumber = "OFF001",
+                PurchaseDate = new DateTime(2019,2,26),
+                PolicyStartDate = new DateTime(2019,2,27)
+            });
+
+
+            var terminateResponse = await client.DoPostAsync<TerminatePolicyResult>("/api/Policies/Terminate", new TerminatePolicyCommand
+            {
+                PolicyId = createPolicyResponse.Data.PolicyId,
+                TerminationDate = new DateTime(2019,6,1)
+            });
+            
+            var confirmTerminateResponse = await client.DoPostAsync<ConfirmTerminationResult>("/api/Policies/ConfirmTermination", new ConfirmTerminationCommand
+            {
+                PolicyId = createPolicyResponse.Data.PolicyId,
+                VersionToConfirmNumber = 2
+            });
+            
+            
+            Assert.Equal(2, confirmTerminateResponse.Data.VersionConfirmed);
+        }
     }
 }
